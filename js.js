cc2b027c685deae7633a03e695aff292218cb041
Karina-Brave-QA <166299783+Karina-Brave-QA@users.noreply.github.com>
2024-04-07 17:17:20 +0300

    Файлы сайта-визитки

---
+++ js.js
@@ -0,0 +1,7 @@
+/* Это объявление переменной, мы наши кнопку по тегу */
+const button = document.querySelector('button');
+
+/* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
+button.addEventListener('click', function() {
+	alert('• Клиент-серверная архитектура •Devtools •Postman •Swagger •Miro •Test IT •Яндекс трекер •SQL •Sentry •Kibana •Grafana')
+})
